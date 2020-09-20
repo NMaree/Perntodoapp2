81296b154bb2c279413b29d81990568753f93c90
NMaree <61126225+NMaree@users.noreply.github.com>
2020-09-20 16:50:49 +1200

    created a server

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const pool = require('./db');
 
-// middleware
+// middleware 
 app.use(cors());
 app.use(express.json());
 
 app.listen(5000, () => {
-    console.log("Server has started on port 5000")
+    console.log("Server is listen on port 5000");
 })
-// routes   
-app.post('/todos', async (req, res) => {
-    try {
-        const { description } = req.body;
-        console.log(req.body);
-        const newTodo = await pool.query('INSERT INTO mytodo (description) VALUES($1) RETURNING *',
-            [description]
-        );
 
-        res.json(newTodo.rows[0]);
-    } catch (err) {
-        console.log(err.message)
-    }
-})
+// routes   ;
+// app.post('/todos', async (req, res) => {
+//     try {
+//         const { description } = req.body;
+//         console.log(req.body);
+//         const newTodo = await pool.query('INSERT INTO mytodo (description) VALUES($1) RETURNING *',
+//             [description]
+//         );
+
+//         res.json(newTodo.rows[0]);
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// })
